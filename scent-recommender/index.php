d98876c93a71eba84694ff06f8182d087f6bd730
Vin Tristan E. Balajadia <balajadiavintristan@gmail.com>
2025-06-21 23:55:43 +0800

    Initial commit

---
+++ scent-recommender/index.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+
+    require_once 'config/config.php';
+?>
+
+<?php include 'views/header.php'; ?>
+<?php include 'views/footer.php'; ?>
+<?php include 'views/home.php';   ?>
+<?php include 'views/results.php';?>
