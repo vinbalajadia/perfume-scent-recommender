2ca7cdecb6bf0ca47c84e3895de090cef76038ff
Vin Tristan E. Balajadia <balajadiavintristan@gmail.com>
2025-06-21 23:58:53 +0800

    Initial commit

---
+++ models/scent.php
@@ -0,0 +1,23 @@
+class Perfume{
+    public $name;
+    public $brand;
+    public $price;
+    public $notes;
+    public $types;
+    public $image;
+    public $rating;
+    public $description;
+
+    public funtion __construct($name, $brand, $price, $notes, $types, $image, $rating, $description) {
+        $this->name = $name;
+        $this->brand = $brand;
+        $this->price = $price;
+        $this->notes = $notes;
+        $this->types = $types;
+        $this->image = $image;
+        $this->rating = $rating;
+        $this->description = $description;
+    }
+
+    public function get
+}
